590cb96c5d464183ad1af707f26754a43f05d54f
MarVoy-360 <drihemzou@gmail.com>
2025-12-02 14:42:19 +0000

    Simplify Prisma client instantiation

--- lib/prisma/client.ts
+++ lib/prisma/client.ts
@@ -1,15 +1,1 @@
-import { PrismaClient } from '@prisma/client';
-
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-// Prevent multiple instances of Prisma Client in development
-export const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
-
-export default prisma;
+import { PrismaClient } from "@prisma/client"; const prisma = new PrismaClient(); export default prisma;
